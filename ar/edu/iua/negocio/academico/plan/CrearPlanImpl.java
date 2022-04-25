d94c8098d4910b46fe7f3e13861c836c0eb85683
guadarodriguez <91908976+guadarodriguez@users.noreply.github.com>
2022-04-25 01:31:24 -0300

    Update CrearPlanImpl.java

--- ar/edu/iua/negocio/academico/plan/CrearPlanImpl.java
+++ ar/edu/iua/negocio/academico/plan/CrearPlanImpl.java
@@ -1,15 +1,31 @@
 package ar.edu.iua.negocio.academico.plan;
 
+import ar.edu.iua.modelo.academico.plan.AnioPlan;
+import ar.edu.iua.modelo.academico.plan.AnioPlanImpl;
 import ar.edu.iua.modelo.academico.plan.Plan;
 
 public class CrearPlanImpl implements CrearPlan {
 
     public boolean crear(Plan plan) {
+        if (plan != null){
+            return false;
+        }
+        if(!plan.isEstadoActivo()|| !plan.isEstadoNoActivo() || plan.isEstadoBorrador()){
+            return false;
+        }
+        if (plan.getAnio()== null && !plan.isEstadoBorrador()){
+            return false;
+        }
+        if(plan.getAnio()<1990 && plan.getAnio()>2040){
+            return false;
+        }
+
+       
 
         //... por favor completar el codigo faltante
         
 
-        return false;
+        return true;
+    }
     }
 
-}
